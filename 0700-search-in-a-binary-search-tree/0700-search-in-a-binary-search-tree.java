81e1e05bb0efdd26d9ee18cb328ab91f53199caf
nirajhomkar <homkarniraj@gmail.com>
2026-07-14 09:21:09 +0530

    Time: 0 ms (100%), Space: 46.6 MB (94.9%) - LeetHub

--- 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.java
+++ 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.java
@@ -27,10 +27,6 @@ class Solution {
         {
             return searchBST(root.left,val);
         }
-        if(root.val < val)
-        {
-            return searchBST(root.right,val);
-        }
-        return new TreeNode();
+        return searchBST(root.right,val);
     }
 }
